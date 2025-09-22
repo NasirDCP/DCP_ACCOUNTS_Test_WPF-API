d1735e48cc9ab508b86cf9a8a26c466c7b1c4dae
Nasir <Nasir@NASIR_DCP>
2025-09-23 03:25:21 +0600

    Add project files.

---
+++ DCP_ACCOUNTS_Test/Model/TblExpendHead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DCP_ACCOUNTS_Test.Model;
+
+public partial class TblExpendHead
+{
+    public string? ExpCode { get; set; }
+
+    public string? ExpDetail { get; set; }
+
+    public string? Uid { get; set; }
+
+    public DateTime? EntryDate { get; set; }
+}
---
+++ DCP_ACCOUNTS_Test/ViewModel/tblExpendViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DCP_ACCOUNTS_Test.ViewModel
+{
+    public class tblExpendViewModel
+    {
+
+    }
+    public class RelayCommand : ICommand {
+        public RelayCommand(Func<object, Task> executeAsync) => _executeAsync = executeAsync;
+        private readonly Func<object, Task> _executeAsync;
+        
+        public event EventHandler? CanExecuteChanged;
+        public bool CanExecute(object? parameter) => true;
+        public async void Execute(object? parameter) => await _executeAsync(parameter!);
+
+    }
+}
